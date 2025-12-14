00840e007a0793c5b9ac50d6d4b0c8e39903e51e
DESKTOP-90KR9TR\9999 <nguyenvancuong.tm@gmail.com>
2025-12-14 11:34:54 +0700

    update code

--- main.js
+++ main.js
@@ -414,52 +414,59 @@
     /*------------------------------------------
         = COUNTDOWN CLOCK
     -------------------------------------------*/
-    if ($("#clock").length) {
-        function timeElapse(date) {
-            var current = Date();
+    if ($('.clock').length) {
+        function timeElapseClock($el, date) {
+            var current = new Date();
             var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
             var days = Math.floor(seconds / (3600 * 24));
             if (days < 10) {
-                days = "0" + days;
+                days = '0' + days;
             }
             seconds = seconds % (3600 * 24);
             var hours = Math.floor(seconds / 3600);
             if (hours < 10) {
-                hours = "0" + hours;
+                hours = '0' + hours;
             }
             seconds = seconds % 3600;
             var minutes = Math.floor(seconds / 60);
             if (minutes < 10) {
-                minutes = "0" + minutes;
+                minutes = '0' + minutes;
             }
             seconds = seconds % 60;
             if (seconds < 10) {
-                seconds = "0" + seconds;
+                seconds = '0' + seconds;
             }
-            var html = '<div class="box"><div>' + days + '</div> <span>' + $('#clock').data('text-day') + '</span></div><div class="box"><div>' + hours + '</div> <span>' + $('#clock').data('text-hour') + '</span> </div><div class="box"><div>' + minutes + '</div> <span>' + $('#clock').data('text-minute') + '</span> </div><div class="box"><div>' + seconds + '</div> <span>' + $('#clock').data('text-second') + '</span></div>';
-            $('#clock').html(html);
+            var html = '<div class="box"><div>' + days + '</div> <span>' + $el.data('text-day') + '</span></div>' +
+                '<div class="box"><div>' + hours + '</div> <span>' + $el.data('text-hour') + '</span> </div>' +
+                '<div class="box"><div>' + minutes + '</div> <span>' + $el.data('text-minute') + '</span> </div>' +
+                '<div class="box"><div>' + seconds + '</div> <span>' + $el.data('text-second') + '</span></div>';
+            $el.html(html);
         }
-        var time = $('#clock').data('date');
-        $('#clock').countdown(time.replace(/-/g, '/'), function (event) {
-            if (event.type == 'stoped') {
-                var together = new Date($('#clock').data('date'));
-                together.setHours(0);
-                together.setMinutes(0);
-                together.setSeconds(0);
-                together.setMilliseconds(0);
-                setInterval(function () {
-                    timeElapse(together);
-                }, 1000);
-            } else {
-                var $this = $(this).html(event.strftime(''
-                    + '<div class="box"><div>%D</div> <span>' + $('#clock').data('text-day') + '</span> </div>'
-                    + '<div class="box"><div>%H</div> <span>' + $('#clock').data('text-hour') + '</span> </div>'
-                    + '<div class="box"><div>%M</div> <span>' + $('#clock').data('text-minute') + '</span> </div>'
-                    + '<div class="box"><div>%S</div> <span>' + $('#clock').data('text-second') + '</span> </div>'));
-            }
+
+        $('.clock').each(function () {
+            var $el = $(this);
+            var time = $el.data('date');
+            // init countdown plugin for this element
+            $el.countdown(time.replace(/-/g, '/'), function (event) {
+                if (event.type == 'stoped') {
+                    var together = new Date($el.data('date'));
+                    together.setHours(0);
+                    together.setMinutes(0);
+                    together.setSeconds(0);
+                    together.setMilliseconds(0);
+                    setInterval(function () {
+                        timeElapseClock($el, together);
+                    }, 1000);
+                } else {
+                    $el.html(event.strftime(''
+                        + '<div class="box"><div>%D</div> <span>' + $el.data('text-day') + '</span> </div>'
+                        + '<div class="box"><div>%H</div> <span>' + $el.data('text-hour') + '</span> </div>'
+                        + '<div class="box"><div>%M</div> <span>' + $el.data('text-minute') + '</span> </div>'
+                        + '<div class="box"><div>%S</div> <span>' + $el.data('text-second') + '</span> </div>'));
+                }
+            });
         });
     }
-
 
     /*------------------------------------------
         = VIDEO BACKGROUND
